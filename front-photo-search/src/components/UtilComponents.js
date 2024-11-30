277ce4ce7f979853e98fa8dfe8a1d3e08bcb6419
Eduardo Sanchez <ants252699@gmail.com>
2024-11-30 00:25:49 -0500

    Use doble quotes

--- front-photo-search/src/components/UtilComponents.js
+++ front-photo-search/src/components/UtilComponents.js
@@ -1,3 +1,3 @@
 export default function Title({ children }) {
-  return <h1 className='font-bold text-xl text-[#4a4a4a]'>{children}</h1>
+  return <h1 className="font-bold text-xl text-[#4a4a4a]">{children}</h1>
 }
